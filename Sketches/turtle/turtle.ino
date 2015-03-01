c8c4bebbea69dcdc6e8e8cc46f05b4ddffe7ce83
Steven Olsen <stevenolsen7@gmail.com>
2015-02-28 22:54:06 -0800

    Initial Commit with Turtle Demo
    New Motor Shield Library. Commented out code related to pins 6 and 12
    which have been disabled on the shield as we don't need them. Removed
    overridden constructor to trim down code size since we don't change the
    default pin map.

---
+++ Sketches/turtle/turtle.ino
@@ -0,0 +1,19 @@
+#include "DualVNH5019MotorShield.h"
+
+DualVNH5019MotorShield motors;
+
+void setup()
+{
+  Serial.begin(115200);
+  motors.init();
+}
+
+void loop()
+{
+  motors.setSpeeds(100, 100);
+  Serial.print("M1 current: ");
+  Serial.println(motors.getM1CurrentMilliamps());
+  Serial.print("M2 current: ");
+  Serial.println(motors.getM2CurrentMilliamps());
+  delay(1000);
+}
