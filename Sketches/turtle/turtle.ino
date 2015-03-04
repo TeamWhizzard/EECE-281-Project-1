e00126f65557d8ed3a8f698fdd8d872eb8e73276
loganfg <logan.gilmore@alumni.ubc.ca>
2015-03-03 19:26:11 -0800

    Added turning functions and updated motorControl in an attempt to go straight.
    Comments that were added should give a description of what the additional functions do.

--- Sketches/turtle/turtle.ino
+++ Sketches/turtle/turtle.ino
@@ -12,8 +12,9 @@ const int pinEchoFront = A3;
 const int pinTemp = A2;
 
 const int MAX_DISTANCE = 300; // maximum reading distance of ultrasonic sensor in cm
-const int DISTANCE_THRESHOLD = 50; // cm
+const int DISTANCE_THRESHOLD = 70; // cm
 const int SPEED_MAX = 255; // PWM speed where 0 is brake, 255 is maximum
+const int MAGIC = 18; // magic number to control difference in left and right motor speed
 
 float temperature; // temperature value in C
 float humidity; // humidity in percentage decimal
@@ -22,6 +23,8 @@ float echoPulseFront; // time returned from ultrasonic sensor
 float velocity; // intermediate ultrasonic sensor calculation
 float time; // intermediate ultrasonic sensor calculation
 float distanceCm; // distance read by ultrasonic sensor
+int value; // used to help vary speeds on motors
+int count = 1; // used in loop() so that delay at beginning only occurs once
 
 DHT dht(pinTemp, DHTTYPE);
 
@@ -78,6 +81,9 @@ void reportCurrent() {
   Serial.print(" ");
 }
 
+// Controls the motors given a speed until a certain distance
+// this version of motorControl() has both motors running at even speeds
+/*
 void motorControl(int velocity, int threshold) {
   if (distanceCm == 0 || distanceCm >= threshold) {
     motors.setSpeeds(velocity, velocity);
@@ -87,21 +93,113 @@ void motorControl(int velocity, int threshold) {
     Serial.print(" ");
     reportDistance();
     reportCurrent();
-    Serial.println("");
+    Serial.println(" ");
   }
+}
+*/
+
+/* 
+Controls the motors given a speed until a certain distance this version of motorControl()
+has both motors running at different speeds to help make the robot go straight
+
+----------------------------------VERY FINICKY----------------------------------
+----------------------------------NOT CONSTANT----------------------------------
+
+
+*/
+void motorControl(int velocity, int threshold) {
+  if (distanceCm == 0 || distanceCm >= threshold) {
+    motors.setSpeeds(velocity, velocity-value); // different velocity values to help it go straight
+  }
+  while (distanceCm == 0 || distanceCm >= threshold) {
+    Serial.print(velocity);
+    Serial.print(" ");
+    reportDistance();
+    reportCurrent();
+    Serial.println(" ");
+  }
+  value *= 0.75; // scale value so that it affects the seperate speeds differently while slowing down
+}
+
+/*
+Controls the turning. Turns left and measures the distance to the left wall, then turns right and measures
+the distance to the right wall. If the distance to the right wall is over one meter or greater than the path
+to the left, the robot moves to the right wall. If not, then it turns back to the left and proceeds down the
+left path
+*/
+void turnControl(){
+ while(distanceCm == 0 || distanceCm <= 5){
+    // turns left first and measures distance
+    reportDistance();
+    turnLeft();
+    reportDistance();
+    float pathLeft = distanceCm;
+    delay(500);
+    
+    // then turns right and measures distance
+    turnRight();
+    reportDistance();
+    float pathRight = distanceCm;
+    delay(500);
+    
+    // decides on which path to take using the logic in the function description
+    if(pathRight >= 1000 || pathRight == 0){
+      break;
+    }
+    else if(pathRight >= pathLeft && pathLeft != 0){
+      break;
+    }
+    else{
+      turnBackLeft();
+      break;
+    }
+  } 
+}
+
+// turns the robot 90 degrees to the left
+void turnLeft(){
+  motors.setSpeeds(100, -100);
+  delay(900);
+  motors.setBrakes(400, 400);
+}
+
+// turns the robot 180 degrees to the right
+void turnRight(){
+  motors.setSpeeds(-100, 100);
+  delay(1730);
+  motors.setBrakes(400, 400);
+}
+
+// turns the robot 180 degrees to the left
+void turnBackLeft(){
+  motors.setSpeeds(100, -100);
+  delay(1730);
+  motors.setBrakes(400, 400);
 }
 
 void loop() {
-  delay(10000);
-  //intro();
-
+  reportDistance();
+  
+  // 1 second delay at the beginning
+  if(count == 1){
+    delay(1000);
+    //intro();
+    count--;
+  }
+  
+  // used to reset value to the magic number "MAGIC"
+  value = MAGIC;
+  
+  // goes through 5 different speeds
   motorControl(400, DISTANCE_THRESHOLD);
-  motorControl(300, 40);
-  motorControl(150, 20);
-  motorControl(100, 10);
-  motorControl(50, 5);
+  motorControl(300, 50);
+  motorControl(150, 35);
+  motorControl(100, 7);
+  motorControl(50, 3);
 
+  // applies the brakes when close enough to the wall
   motors.setBrakes(400, 400);
-
-  //delay(10000);
+  
+  turnControl();
+  delay(500);
 }
