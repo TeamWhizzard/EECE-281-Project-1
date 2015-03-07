87a6807e2aec86a79189fe18db47429f75a3e0ed
Anthony C <broac@live.ca>
2015-03-07 14:14:36 -0800

    Updated controller logic
    -Fixed countRotate and loop interaction (only delay inside countRotate, this way it can constantly check if the user is moving without delay)
    -Modularized loop logic into separate methods 
    -Commented absolutely everything

--- Sketches/controller/controller.ino
+++ Sketches/controller/controller.ino
@@ -2,21 +2,19 @@
 // By Arduino User JohnChi
 // August 17, 2014
 // Public Domain
-#include "DualVNH5019MotorShield.h"
+
 #include<Wire.h>
 const int MPU=0x68;  // I2C address of the MPU-6050
-const int readDelay = 50;
-const int RIGHT_THRESHOLD = -4000;
-const int LEFT_THRESHOLD = 4000;
-const int FORWARD_THRESHOLD = 4000;
-const int SLOWDOWN_THRESHOLD = -4000;
-const int RIGHT_END_BOUNDARY = -90;
-const int RIGHT_START_BOUNDARY = -20;
-const int LEFT_START_BOUNDARY = 20;
-const int LEFT_END_BOUNDARY = 90;
-const float FS_ZERO_GYRO_SCALE = 131;
-float totalDegrees = 0;
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+const int readDelay = 50;   //Delay to read new acceleration/angular velocity values
+const int FORWARD_THRESHOLD = 4000;   //Acceleration (scaled) threshold value to speed up
+const int SLOWDOWN_THRESHOLD = -4000;  //Accereration (scaled) threshold value to slow down
+const int RIGHT_END_BOUNDARY = -90;  //End of degree boundary to indicate turning right
+const int RIGHT_START_BOUNDARY = -20;  //Start of degree boundary indicate turning right
+const int LEFT_START_BOUNDARY = 20;  //Start of degree boundary to indicate turning left
+const int LEFT_END_BOUNDARY = 90;  //End of degree boundary to indicate turning left 
+const float FS_ZERO_GYRO_SCALE = 131;  //Scale factor of angular velocity readings
+float totalDegrees = 0;  //Variable to keep track of total degrees turned, with the controller centered being 0 degrees
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;  //Variables to keep track of acceleration/gyroscope values in all axises
 
 void setup(){
       Wire.begin();
@@ -26,6 +24,8 @@ void setup(){
       Wire.endTransmission(true);
       Serial.begin(9600);
 }
+
+//Updates all acceleration and angular velocity readings
 void readAll(){
       Wire.beginTransmission(MPU);
       Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
@@ -40,6 +40,8 @@ void readAll(){
       GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
 }
+
+//Prints all the acceleration and angular velocity readings
 void printAll(){
       Serial.print("AcX = "); Serial.print(AcX);
       Serial.print(" | AcY = "); Serial.print(AcY);
@@ -49,68 +51,53 @@ void printAll(){
       Serial.print(" | GyY = "); Serial.print(GyY);
       Serial.print(" | GyZ = "); Serial.println(GyZ);
 }
+
+//Counts the degrees rotated 
 void countRotate() {
-  float prevDegrees;
-  float currDegrees;
   do {
-    readAll();
-    float GyXdps = GyX / FS_ZERO_GYRO_SCALE;
-    Serial.print("Degrees per Second: ");
-    Serial.println(GyXdps);
-    float currDegrees = GyXdps * readDelay / 1000;
-    Serial.print("Total Degrees turned: ");
-    if (abs(currDegrees) >= 0.7){
-      if (prevDegrees == 0)//Start will have no prevDegrees. Slight accuracy increase. (Edge case for trap rule)
-        totalDegrees += currDegrees;
-      else //Trapezoidal rule
-        totalDegrees += (prevDegrees+currDegrees)/2;
+    readAll();  //Update all accel/gyro values
+    float GyXdps = GyX / FS_ZERO_GYRO_SCALE;  //Convert the gyro reading to degrees per second
+    float currDegrees = GyXdps * readDelay / 1000;  //Perform approximate integration by multiplying degree per second with the delay
+    if (abs(currDegrees) >= 0.7){  //Gyro readings hover below 0.7 dps at rest, don't take these values
+        totalDegrees += currDegrees  //Iteratively sum the current degrees into total degrees
     }
-    Serial.println(totalDegrees);
-    prevDegrees = currDegrees;
-    delay(readDelay);
-  } while (abs(currDegrees) >= 0.7)
+    Serial.print("Degrees per Second: ");  //Print out the dps reading
+    Serial.println(GyXdps);
+    Serial.print("Total Degrees turned: ");  //Print out total degrees turned so far
+    Serial.println(totalDegrees);  
+    delay(readDelay);    //Delay for the given duration inbetween reads
+  } while (abs(currDegrees) >= 0.7)  //Only loop while the controller is moving (only reading inside loop, hence do while)
 }
+
 //Checks for a turn motion and sets motors to turn appropriately.
 void checkTurnControl(){
-//  if (AcZ <= RIGHT_THRESHOLD){
-//    Serial.println("TURNING RIGHT");
-//    delay(1500);
-//    Serial.println("TURN COMPLETE");
-//    
-//  }
-//  else if (AcZ >= LEFT_THRESHOLD){
-//    Serial.println("TURNING LEFT");
-//    delay(1500);
-//    Serial.println("TURN COMPLETE");
-//  }
-  
+  readAll();
   if (AcY >= FORWARD_THRESHOLD){
     Serial.println("SPEEDING UP");
     delay(1500);
   }
-  
   else if (AcY <= SLOWDOWN_THRESHOLD){
     Serial.println("SLOWING DOWN");
     delay(1500);
   }
-
 }
-
-
-void loop(){
-      readAll();
-      //checkTurnControl();
-      countRotate();
+void checkRotateControl(){
+      //In the left boundary zone? Turn robot left while controller in left zone
       if (totalDegrees >= LEFT_END_BOUNDARY || totalDegrees <= LEFT_START_BOUNDARY){
         Serial.print("TURNING LEFT");
       }
+      //In the center boundary zone? Center the robot while controller in center zone
       else if (totalDegrees > LEFT_START_BOUNDARY || totalDegrees < RIGHT_START_BOUNDARY){
         Serial.print("CENTERED");
       }
+      //In the right boundary zone? Turn robot right while controller in right zone. 
       else if (totalDegrees >= RIGHT_START_BOUNDARY || totalDegrees <= RIGHT_END_BOUNDARY){
         Serial.print("TURNING RIGHT");
       }
-      printAll();
-      delay(readDelay);
 }
 
+void loop(){
+      countRotate();  //While the controller is moving, record total degrees that the controller turned
+      checkRotateControl();  //Perform the appropriate action on the robot according to degrees turned
+      printAll();  //Print accellerometer/gyroscope reading values
+}
