991073d794725856a4213a43c158c651acf5ca09
tmammare <mammarellatheresa8@gmail.com>
2015-03-10 17:57:34 -0700

    updates

--- Old Sketches/DFRobot_Encoder_Test/DFRobot_Encoder_Test.ino
+++ Old Sketches/DFRobot_Encoder_Test/DFRobot_Encoder_Test.ino
@@ -22,15 +22,17 @@
 double input;
 double output;
 double target;
-double kp = 2;
-double ki = 2;
-double kd = 0;
+double kp = 2; // dependent on present error
+double ki = 5; // accumulation of past error
+double kd = 1; // prediction of future error based on current rate of change
 
 //volatile long coder[2] = {0, 0};
 volatile long coder[2] = {0, 0}; // set left motor to -206 to offset shield triggering interrupt
 int lastSpeed[2] = {0, 0};
 
 int mspeed = 350;
+
+bool delayFlag = 1;
 
 static unsigned long timer = 0;                //print manager timer
 
@@ -45,22 +47,25 @@ void setup() {
   for (int i = 4; i <= 7; i++) {                 // Motor Pin Assignments
     pinMode(i, OUTPUT);
   }
-  attachInterrupt(LEFT, LwheelSpeed, RISING);    //init the interrupt mode for the digital pin 2
-  attachInterrupt(RIGHT, RwheelSpeed, RISING);   //init the interrupt mode for the digital pin 3
-  
- // coder[LEFT] = 0;
-  
-  motors.setSpeeds(mspeed, mspeed);
-  
+  attachInterrupt(0, LwheelSpeed, CHANGE);    //init the interrupt mode for the digital pin 2
+  attachInterrupt(1, RwheelSpeed, CHANGE);   //init the interrupt mode for the digital pin 3
   
   myPid.SetOutputLimits(-90, 90);
   myPid.SetMode(AUTOMATIC); // turn on pid
   
-  //delay(5000);
-  //target = 0; // stay forward at zero degrees
+  target = 0; // stay forward at zero degrees
 }
 
 void loop() {
+  if (delayFlag) {
+    if (millis() > 10000) { // start PID once noise has settled
+      coder[LEFT] = 0;
+      coder[RIGHT] = 0;
+      motors.setSpeeds(mspeed, mspeed);
+      delayFlag = 0;
+    }
+  }
+  
   //static unsigned long timer = 0;                //print manager timer
 
   //if(millis() - timer > 100){                   
@@ -76,8 +81,8 @@ void loop() {
     
     input = coder[RIGHT] - coder[LEFT];
     
-    myPid.Compute();
-    motors.setSpeeds(mspeed - output, mspeed + output);
+    //myPid.Compute();
+    //motors.setSpeeds(mspeed - output, mspeed + output);
 
     //lastSpeed[LEFT] = coder[LEFT];   //record the latest speed value
     //lastSpeed[RIGHT] = coder[RIGHT];
