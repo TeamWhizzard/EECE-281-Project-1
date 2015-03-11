0d863ba16ca6278c7091c547575390cc3d0e2873
tmammare <mammarellatheresa8@gmail.com>
2015-03-10 19:47:31 -0700

    pid passoff

--- Old Sketches/DFRobot_Encoder_Test/DFRobot_Encoder_Test.ino
+++ Old Sketches/DFRobot_Encoder_Test/DFRobot_Encoder_Test.ino
@@ -14,35 +14,35 @@
 // #
 
 #include "PID_v1.h"
-#include "DualVNH5019MotorShield.h"
+//#include "DualVNH5019MotorShield.h"
 
-#define LEFT 0
-#define RIGHT 1
+#define RIGHT 0
+#define LEFT 1
+
+// Motors
+#define RIGHT_MOTOR_CONTROL 4 // Right motor Direction Control
+#define RIGHT_MOTOR_SPEED 5 // Right motor Speed Control
+#define LEFT_MOTOR_CONTROL 7 // Left motor Direction Control
+#define LEFT_MOTOR_SPEED 6 // Left motor Speed Contro
 
 double input;
 double output;
-double target;
+double target = 0;
 double kp = 2; // dependent on present error
 double ki = 5; // accumulation of past error
 double kd = 1; // prediction of future error based on current rate of change
 
-//volatile long coder[2] = {0, 0};
-volatile long coder[2] = {0, 0}; // set left motor to -206 to offset shield triggering interrupt
-int lastSpeed[2] = {0, 0};
+volatile long coder[2] = {0, 0};
+//int lastSpeed[2] = {0, 0};
 
-int mspeed = 350;
-
-bool delayFlag = 1;
+int mspeed = 175;
 
 static unsigned long timer = 0;                //print manager timer
-
-DualVNH5019MotorShield motors;
 
 PID myPid(&input, &output, &target, kp, ki, kd, DIRECT);
 
 void setup() {
   Serial.begin(9600);                            //init the Serial port to print the data
-  motors.init();
   
   for (int i = 4; i <= 7; i++) {                 // Motor Pin Assignments
     pinMode(i, OUTPUT);
@@ -50,57 +50,40 @@ void setup() {
   attachInterrupt(0, LwheelSpeed, CHANGE);    //init the interrupt mode for the digital pin 2
   attachInterrupt(1, RwheelSpeed, CHANGE);   //init the interrupt mode for the digital pin 3
   
-  myPid.SetOutputLimits(-90, 90);
-  myPid.SetMode(AUTOMATIC); // turn on pid
-  
-  target = 0; // stay forward at zero degrees
+  myPid.SetOutputLimits(-50, 50);
+  myPid.SetMode(AUTOMATIC); // pid is driving, to start
 }
 
-void loop() {
-  if (delayFlag) {
-    if (millis() > 10000) { // start PID once noise has settled
-      coder[LEFT] = 0;
-      coder[RIGHT] = 0;
-      motors.setSpeeds(mspeed, mspeed);
-      delayFlag = 0;
-    }
-  }
-  
-  //static unsigned long timer = 0;                //print manager timer
-
-  //if(millis() - timer > 100){                   
-    Serial.print("Coder value: ");
+void loop() {                   
+    Serial.print("Encoders: ");
     Serial.print(coder[LEFT]);
-    Serial.print("[Left Wheel] ");
-    Serial.print(coder[RIGHT]);
-    Serial.print("[Right Wheel]");
-    Serial.print(mspeed - output);
-    Serial.print("[Left Wheel] ");
-    Serial.print(mspeed + output);
-    Serial.println("[Right Wheel]");
+    Serial.print(" ");
+    Serial.println(coder[RIGHT]);
+    Serial.print("Speeds: ");
+    Serial.print(leftSpeed);
+    Serial.print(" ");
+    Serial.println(rightSpreed);
     
     input = coder[RIGHT] - coder[LEFT];
-    
-    //myPid.Compute();
-    //motors.setSpeeds(mspeed - output, mspeed + output);
-
-    //lastSpeed[LEFT] = coder[LEFT];   //record the latest speed value
-    //lastSpeed[RIGHT] = coder[RIGHT];
-    //coder[LEFT] = 0;                 //clear the data buffer
-    //coder[RIGHT] = 0;
-    //timer = millis();
-  //}
+    myPid.Compute(); // updates PID output
+    leftSpeed = mspeed + output;
+    rightSpeed = mspeed - output;
+    forward(leftSpeed, rightSpeed);
+    delay(100);
 }
 
-void LwheelSpeed()
-{
+void LwheelSpeed() {
   coder[LEFT] ++;  //count the left wheel encoder interrupts
 }
 
 
-void RwheelSpeed()
-{
+void RwheelSpeed() {
   coder[RIGHT] ++; //count the right wheel encoder interrupts
 }
 
-
+void forward(int leftMotor, int rightMotor) {
+  analogWrite (5, rightMotor); //PWM Speed Control (0-255)
+  digitalWrite(4, HIGH); // HIGH = moves forwards
+  analogWrite (6, leftMotor); //PWM Speed Control (0-255)
+  digitalWrite(7, HIGH);  // HIGH = moves forwards
+}
